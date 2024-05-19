6d6b99145e7c6b6223ddf186042c873d4b393746
Lucy <110852410+epistemophoros@users.noreply.github.com>
2024-05-19 14:35:00 -0400

    Update sort-phishing.sh

--- sort-phishing.sh
+++ sort-phishing.sh
@@ -11,8 +11,8 @@ base_name="$1"
 
 # Define the input and output file names based on the provided website name
 input_file="phishing-${base_name}.txt"
-active_file="${base_name}-active.txt"
-unactive_file="${base_name}-unactive.txt"
+active_file="phishing-${base_name}-active.txt"
+unactive_file="phishing-${base_name}-unactive.txt"
 
 # Check if the input file exists
 if [ ! -f "$input_file" ]; then
